cdc8de5a63e64dfb5d71ce112d589ca4c9ef8244
vvvirgy <148044855+vvvirgy@users.noreply.github.com>
2025-02-25 09:51:57 +0100

    names remapping

--- R/mapping_names.R
+++ R/mapping_names.R
@@ -10,7 +10,30 @@ proteomics_data = proteomics_data %>%
 mapping = readxl::read_excel("mapping_ICR_names.xlsx")
 
 mapping = mapping %>% 
-  select(`Patient code (HSR)`, `Patient code (ICR)`, fixed_name)
+  select(`Patient code (HSR)`, `Patient code (ICR)`, fixed_name) %>% 
+  dplyr::rename(proteomics_code = `Patient code (HSR)`) %>% 
+  dplyr::rename(genomics_code = `Patient code (ICR)`) %>% 
+  dplyr::mutate(proteomics_code = gsub("#", "", proteomics_code))
+  
+mapping_bis = tibble(
+  sample_proteomics_code = colnames(proteomics_data)[-1], 
+  genomics_code = gsub("_a|_b", "", colnames(proteomics_data)[-1])
+  # fixed_name = colnames(proteomics_data)[-1]
+)
+
+tt = full_join(mapping, mapping_bis, by = join_by("proteomics_code" == "genomics_code"))
+  
+new_mapping = tt %>% 
+  mutate(fixed_name = ifelse(is.na(fixed_name), gsub("HSR", "", proteomics_code), fixed_name)) %>% 
+  mutate(fixed_name = gsub("-", "_", fixed_name)) 
+
+new_mapping_experiment = new_mapping %>% 
+  dplyr::filter(!is.na(sample_proteomics_code)) %>% 
+  dplyr::mutate(fixed_name = gsub("_seg", "", fixed_name)) %>% 
+  dplyr::mutate(fixed_name = case_when(fixed_name == "65_VI" ~ "65_IV", 
+                                       .default = fixed_name))
+
+saveRDS(new_mapping_experiment, "mapping_samples.rds")
 
 # select the top genes to check first
 genes_to_check = c("APC", "KRAS", "TP53", "PIK3CA")
@@ -18,14 +41,17 @@ genes_to_check = c("APC", "KRAS", "TP53", "PIK3CA")
 drivers_to_check = drivers %>% 
   dplyr::filter(gene %in% genes_to_check)
 
+drivers_to_check_correct_samples = drivers_to_check %>% 
+  filter(PDO %in% new_mapping_experiment$fixed_name %>% unique)
+
 # add the mapping to the proteomics ids
-new_names_data = dplyr::full_join(drivers_to_check, mapping, by = join_by("PDO" == "fixed_name"))
+# new_names_data = dplyr::full_join(drivers_to_check, mapping, by = join_by("PDO" == "fixed_name"))
 
 # CNA info 
-new_names_data %>% 
-  select(gene, karyotype, PDO) %>% 
-  full_join(., proteomic_genes, by = "PDO")
-
+# drivers_to_check_correct_samples %>% 
+#   select(gene, karyotype, PDO) %>%
+#   full_join(., new_mapping_experiment, by = join_by("PDO" == "fixed_name"))
+  
 # Filter the data to keep only selected genes
 proteomic_to_check = proteomics_data %>%
   tidyr::separate(gene, into = c("uniprot", "gene_name"), sep = "_") %>% 
@@ -40,19 +66,33 @@ proteomic_genes = proteomic_genes %>%
   dplyr::mutate(PDO = gsub("_a|_b", "", sample_id)) %>% 
   dplyr::group_by(PDO, gene_name) %>% 
   dplyr::summarise(mean_expr = mean(value), sd_expr = sd(value)) 
-  
+
+samples_check = new_mapping_experiment %>% 
+  select(proteomics_code, fixed_name) %>%
+  distinct
+
+x = drivers_to_check_correct_samples %>%
+  select(gene, karyotype, PDO) %>% 
+  distinct(.keep_all = F) %>% 
+  left_join(., samples_check, by = join_by("PDO" == "fixed_name")) %>% 
+  # filter(!is.na(gene)) %>% 
+  full_join(., proteomic_genes, by = join_by("proteomics_code" == "PDO", "gene" == "gene_name")) %>% 
+  mutate(karyotype = ifelse(is.na(karyotype), "no alteration", karyotype))
+
+
 # first visualization
-proteomic_genes %>% 
-  ggplot(aes(x = reorder(gene_name, +mean_expr), y = mean_expr, fill = reorder(PDO, +mean_expr))) +
-  geom_bar(stat = "identity", position = "dodge") + 
-  facet_wrap(~gene_name, scales = "free") 
 
-# add the CNA info
-drivers_to_check$PDO %>% unique() %>% length
+x = x %>% 
+  mutate(karyotype = factor(karyotype, 
+                            levels = c("no alteration", "1:1", "1:0", "2:0", "2:1", "2:2", "3:0", "3:1", "3:2", "3:3", "4:0", "5:0")))
 
-samples_proteomics = lapply(strsplit(x = proteomic_genes$variable %>% unique %>% as.character, split = "_"), dplyr::first) %>% 
-  unlist() %>% unique %>% 
-  gsub("-", "_", .)
+cna_colors = setNames(nm = x$karyotype %>% levels, object = c("gainsboro", "#BBE2EC", yarrr::piratepal(palette = "basel")))
 
-
+x %>% 
+  ggplot(aes(x = karyotype, y = mean_expr, fill = karyotype)) + 
+  geom_boxplot(outliers = F) + 
+  facet_wrap(vars(gene), scales = "free") + 
+  geom_jitter() + 
+  theme_bw() + 
+  scale_fill_manual(values = cna_colors)
 
