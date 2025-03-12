ecbc1ec90d178119ea11e9c76dd0daf5278d76ee
Virginia Gazziero <virginiagazziero@gmail.com>
2025-03-12 12:23:11 +0100

    Update mapping_names.R

--- R/mapping_names.R
+++ R/mapping_names.R
@@ -70,7 +70,13 @@ drivers_to_check_correct_samples = drivers_to_check_correct_samples %>%
 # drivers_to_check_correct_samples %>% 
 #   select(gene, karyotype, PDO) %>%
 #   full_join(., new_mapping_experiment, by = join_by("PDO" == "fixed_name"))
-  
+proteomics_data %>%
+  rename(x = '11HSR_a') %>% 
+  mutate(x = as.numeric(x)) %>% 
+  ggplot(aes(x)) +
+  geom_histogram()
+
+
 # Filter the data to keep only selected genes
 proteomic_to_check = proteomics_data %>%
   tidyr::separate(gene, into = c("uniprot", "gene_name"), sep = "_") %>% 
@@ -83,15 +89,15 @@ proteomic_genes = proteomic_to_check %>%
 proteomic_genes = proteomic_genes %>% 
   dplyr::rename(sample_id = variable) %>% 
   dplyr::mutate(PDO = gsub("_a|_b", "", sample_id)) %>% 
-  dplyr::group_by(PDO, gene_name) %>% 
-  dplyr::summarise(mean_expr = mean(value), sd_expr = sd(value)) 
+  dplyr::group_by(PDO, gene_name) #%>% 
+  # dplyr::summarise(mean_expr = mean(value), sd_expr = sd(value)) 
 
 samples_check = new_mapping_experiment %>% 
   select(proteomics_code, fixed_name) %>%
   distinct
 
 x = drivers_to_check_correct_samples %>%
-  select(gene, karyotype, PDO, multiplicity, Consequence) %>% 
+  select(gene, karyotype, PDO, multiplicity, Consequence, Major, minor) %>% 
   # mutate(state = paste0(karyotype, "-", multiplicity)) %>% 
   distinct(.keep_all = F) %>% 
   left_join(., samples_check, by = join_by("PDO" == "fixed_name")) %>% 
@@ -111,7 +117,7 @@ x = x %>%
 
 multihit_identifiers = x %>% 
   group_by(gene, karyotype, PDO, multiplicity) %>% 
-  count
+  dplyr::count()
 
 x = full_join(x, multihit_identifiers, by = join_by("gene" == "gene", "karyotype" == 'karyotype', "PDO" == 'PDO', 'multiplicity' == 'multiplicity'))
 x = x %>% 
@@ -122,6 +128,50 @@ x = x %>%
   mutate(Consequence = case_when(Consequence %in% c("frameshift_variant","inframe_deletion") ~ "frame_alteration", 
                                  .default = Consequence))
   
+
+x = x %>% 
+  dplyr::mutate(tot_cna = Major+minor)
+
+colors = setNames(ggthemes::calc_pal()(11), nm = x$karyotype %>% unique)
+
+x %>% 
+  # filter(gene=='KRAS') %>%
+  # filter(karyotype %in% c('1:1', '1:0', '2:0', '2:1', '3:0', '2:2', '3:1')) %>%
+  mutate(expr_by_cna = value/tot_cna) %>% 
+  ggplot(aes(x = expr_by_cna, y = tot_cna, colour = karyotype)) +
+  geom_point() +
+  theme_bw() +
+  facet_wrap(~gene, scales = 'free') +
+  # scale_color_brewer(palette = 'Paired')
+  # ggsci::scale_color_futurama()
+  scale_color_manual(values = colors)
+
+
+x %>% 
+  ggplot(aes(value, fill = karyotype)) +
+  geom_histogram(binwidth = 0.1) +
+  
+  # filter(gene=='KRAS') %>%
+  # filter(karyotype %in% c('1:1', '1:0', '2:0', '2:1', '3:0', '2:2', '3:1')) %>%
+  # mutate(expr_by_cna = value/tot_cna) %>% 
+  # ggplot(aes(x = expr_by_cna, y = tot_cna, colour = karyotype)) +
+  # geom_point() +
+  theme_bw() +
+  facet_wrap(~gene, scales = 'free') 
+  # scale_color_brewer(palette = 'Paired')
+  # ggsci::scale_color_futurama()
+  scale_color_manual(values = colors)
+
+ggsave('ratio_proteomics_tot_cna_all.png')
+
+x %>% 
+  # filter(gene=='KRAS') %>% 
+  mutate(expr_by_cna = value/tot_cna) %>% 
+  ggplot(aes(expr_by_cna, fill = karyotype)) +
+  geom_histogram(binwidth = 0.5)+
+  theme_bw() +
+  facet_wrap(~gene, scales = 'free') +
+  scale_fill_brewer(palette = 'Paired')
 
 # cna_colors = setNames(nm = x$karyotype %>% levels, object = c("gainsboro", "#BBE2EC", yarrr::piratepal(palette = "basel")))
 
@@ -143,7 +193,7 @@ with_mult
 cna_colors = setNames(nm = x$karyotype %>% levels, object = c("gainsboro", "#BBE2EC", yarrr::piratepal(palette = "basel")))
 
 without_mult = x %>% 
-  ggplot(aes(x = karyotype, y = mean_expr, fill = karyotype)) + 
+  ggplot(aes(x = karyotype, y = value, fill = karyotype)) + 
   geom_boxplot(outliers = F, alpha = 0.8) +
   # geom_point() +
   facet_wrap(vars(gene), scales = "free") + 
