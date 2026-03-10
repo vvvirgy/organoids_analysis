191f98eacb78458a2dde248d4d3d86b55d0d58fd
jovoni <santacatterinagiovanni@gmail.com>
2026-03-10 14:18:02 +0100

    First jovoni commit w analysis and images

---
+++ jovoniR/constants.R
@@ -0,0 +1,16 @@
+
+omic_colors = c(
+  "DNA" = "#66c2a5",
+  "RNA" = "#fc8d62",
+  "Protein" = "#8da0cb"
+)
+
+category_colors <- c(
+  "Compensated" = "#AD002AB2",
+  "Prot-comp." = "#E18727B2",
+  "RNA-comp." = "#20854Eb2",
+  "Hyper-responders" = "#00468BB2",
+  "Intermediate/Other" = "gainsboro"
+)
+
+genes_to_plot = c('MERTK', 'KRAS', 'PTK7', 'FYN', 'ABCB1', 'CDX2', 'APOBEC3C')
